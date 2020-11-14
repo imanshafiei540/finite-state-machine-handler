57cc431204bc229eab72021df5b2bb611f8ded41
imanshafiei <imanshafiei@ymail.com>
2020-11-14 14:31:55 +0330

    Input class add.

---
+++ state_machine/Input.py
@@ -0,0 +1,7 @@
+class Input(object):
+    """
+    This class holds for Input with range value and unit.
+    """
+    def __init__(self, unit, range_value):
+        self.unit = unit
+        self.range_value = range_value
